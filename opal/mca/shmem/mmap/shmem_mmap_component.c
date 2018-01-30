3d72cf482307bb7a0bb3488425b26643460b56fb
Harumi Kuno <harumi.kuno@hpe.com>
2018-01-29 19:24:05 -0800

    MPI_Win_allocate_shared() support for fabric-shared memory.

--- opal/mca/shmem/mmap/shmem_mmap_component.c
+++ opal/mca/shmem/mmap/shmem_mmap_component.c
@@ -134,7 +134,7 @@ mmap_register(void)
         return ret;
     }
 
-    opal_shmem_mmap_relocate_backing_file = 0;
+    opal_shmem_mmap_relocate_backing_file = 1;
     ret = mca_base_component_var_register (&mca_shmem_mmap_component.super.base_version,
                                            "relocate_backing_file",
                                            "Whether to change the default placement of backing files or not "
@@ -150,7 +150,7 @@ mmap_register(void)
         return ret;
     }
 
-    opal_shmem_mmap_backing_file_base_dir = "/dev/shm";
+    opal_shmem_mmap_backing_file_base_dir = "/lfs";
     ret = mca_base_component_var_register (&mca_shmem_mmap_component.super.base_version,
                                            "backing_file_base_dir",
                                            "Specifies where backing files will be created when "
